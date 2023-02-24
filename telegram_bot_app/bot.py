f5721d00bc9717a619f473f8f4332d2593f08d14
Polina <36280649+rabarbra@users.noreply.github.com>
2023-02-24 04:39:16 +0400

    feat: Inline keyboard for poll
    * rebase commit
    
    * feat: inline keyboard
    
    * feat: inline keyboard
    
    * feat: inline keyboard

--- telegram_bot_app/bot.py
+++ telegram_bot_app/bot.py
@@ -5,10 +5,9 @@ CLEANUP_INTERVAL = 60*60
 import logging, dotenv, os, sys, threading, time
 import redis
 
-
 from aiogram import Bot, Dispatcher, executor, types
-# import telebot
-
+import json
+from typing import cast
 
 # Local imports
 import msgs
@@ -94,11 +93,58 @@ async def send_welcome(message):
     await message.reply(msgs.welcome_msg)
     await message.reply(msgs.values)
 
-    
+@dp.message_handler(commands=['poll'])
+async def send_poll(message):
+    await message.reply(msgs.poll_msg, reply_markup=msgs.poll)
+
+@dp.callback_query_handler()
+async def callback_query(call):
+    data = json.loads(call.data)
+    chat_id = call['message']['chat']['id']
+    message_id = call['message']['message_id']
+    keyboard = call['message']['reply_markup']['inline_keyboard']
+    selected = [
+        json.loads(button[0]['callback_data']).get('selected', 0)
+        for button in keyboard
+    ]
+    max_selected = max(selected)
+    if data['type'] == 'answer':
+        if max_selected >= 6:
+            return
+        key = data['key']
+        new_button = keyboard[key][0]
+        callback_data = json.loads(new_button['callback_data'])
+        if callback_data['selected'] > 0:
+            return
+        new_button['text'] = f'({max_selected + 1})  ' + new_button['text']
+        callback_data['selected'] = max_selected + 1
+        new_button['callback_data'] = json.dumps(callback_data)
+        keyboard[key][0] = new_button
+        keyb_markup = types.InlineKeyboardMarkup(inline_keyboard=keyboard)
+        await bot.edit_message_reply_markup(chat_id, message_id, reply_markup=keyb_markup)
+    elif data['type'] == 'clear':
+        new_keyboard = []
+        for button in keyboard:
+            button[0]['text'] = button[0]['text'].split(') ')[-1]
+            callback_data = json.loads(button[0]['callback_data'])
+            if callback_data.get('selected', 0) > 0:
+                callback_data['selected'] = 0
+                button[0]['callback_data'] = json.dumps(callback_data)
+            new_keyboard.append(button)
+        keyb_markup = types.InlineKeyboardMarkup(inline_keyboard=new_keyboard)
+        await bot.edit_message_reply_markup(chat_id, message_id, reply_markup=keyb_markup)
+    elif data['type'] == 'ok':
+        result = [(key, val) for key, val in enumerate(selected) if val != 0]
+        result.sort(key=lambda x: x[1])
+        if max_selected < 6:
+            return
+        user_info = get_user_info(call['message'])
+        database_user_key = f"user_{user_info['user_id']}"
+        #redis_tools.save_polling_result(redis_connection, database_user_key, [val[0] for val in result])
+        await bot.send_message(chat_id, msgs.after_poll_msg)
 
 ### ADD YOUR ENDPOINTS HERE ###
-    
-    
+
 # Start the bot
 
 if __name__ == '__main__':
